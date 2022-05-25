6cccb263c1dc77161fff27d45fcf02a2851004c2
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-05-26 02:26:52 +0600

    increase case number

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('I am in a separate file')
+document.getElementById("case-plus").addEventListener("click", function () {
+  const caseInput = document.getElementById("case-number");
+  const caseNumber = parseInt(caseInput.value);
+  caseInput.value = caseNumber + 1;
+});
